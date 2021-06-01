f954d5d66e1c76e55e206de1e705e55a95fa7ddb
Alexander Burdiss <aburdiss@icloud.com>
2021-06-01 17:51:36 -0400

    Switched to HashRouter so that saved links will work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./Components/Structural/Header/Header";
 import Footer from "./Components/Structural/Footer/Footer";
