95d12f81122692e7a9c546bbd00c16d2e5d46af6
Alexander Burdiss <aburdiss@icloud.com>
2021-06-05 14:49:44 -0400

    Added 404 Page

--- src/Pages/404/404.js
+++ src/Pages/404/404.js
@@ -1,7 +1,35 @@
+import React from "react";
+import { shuffle } from "underscore";
+
+import ForestMountains from "../../Components/Background/ForestMountains/ForestMountains";
+
+import scroll from "./scroll.svg";
+import oneLiners from "./404OneLiners";
+
+/**
+ * @function FourOhFour
+ * @description The page that is displayed when the URL the user is searching
+ * for doesn't exist on the site.
+ * @author Alexander Burdiss
+ * @since 6/5/21
+ * @version 1.0.0
+ */
 export default function FourOhFour() {
+  function getRandomOneLiner() {
+    const tempOneLiners = shuffle(oneLiners);
+    return tempOneLiners[0];
+  }
+
   return (
-    <div>
-      <div>Hello, 404!</div>
+    <div className="FourOhFour-Container">
+      <ForestMountains />
+      <img src={scroll} alt="" className="FourOhFour-Scroll" />
+      <div className="FourOhFour-Scroll-Text-Container">
+        <h1>404 Error</h1>
+        <p>Whoops oops unfortunately this page was not found.</p>
+        <p>{getRandomOneLiner()}</p>
+        <p>Sincerely, Fostywally</p>
+      </div>
     </div>
   );
 }
---
+++ src/Pages/404/404OneLiners.js
@@ -0,0 +1,11 @@
+export default [
+  "Wildebeest stampede came through and wiped out the museum that was holding the story.",
+  "We tried a new framing technique where we stapled the story to a telephone pole, but they all blew away.",
+  "Our office building is constantly rearranging itself, so the office that was here is now over in the gym, and the gym is in the kitchen, and the tennis team is currently using all the apples as balls. ",
+  "Lionel The Loathesome melted the iceberg our story was being kept on. ",
+  "The resident site weevils are swarming currently, an exterminator has been called. All should be well soon.",
+  "The most recent of the Aviantors flying spectacles has blown all our paper everywhere. We are in the process of picking it up. If you happen to see some ants flying arund in a paper airplane, please tell them to leave us alone. ",
+  "A dense fog has covered our workshop. We have every available unit fanning, but it may be a while.",
+  "Captain Code sneezed and accidently put this part of the site a few miles down the road. Gimme a minute, I’ll go get it. ",
+  "It exploded… Just got the glue and duct tape. Give me a little while to put this back together.",
+];
