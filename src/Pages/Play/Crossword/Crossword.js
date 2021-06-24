7b1eae5cd8649c305f55b862a53fb50984a97d15
Alexander Burdiss <aburdiss@icloud.com>
2021-06-23 22:39:02 -0400

    Finished adding first crossword game. Added README

--- src/Pages/Play/Crossword/Crossword.js
+++ src/Pages/Play/Crossword/Crossword.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Crossword.scss";
 import ReactCrossword from "./ReactCrossword/javascripts/crosswords/crossword";
 
 import PageTitle from "../../../Components/Structural/PageTitle/PageTitle";
-import { games } from "./games";
+import { GrowingGreen } from "./GrowingGreenCW";
 
+/**
+ * @function Crossword
+ * @description A component that handles playing crossword games
+ * @author Alexander Burdiss
+ * @since 6/23/21
+ * @version 1.0.0
+ * @component
+ * @example
+ * ```jsx
+ * <Crossword />
+ * ```
+ */
 export default function Crossword() {
+  // eslint-disable-next-line no-unused-vars
+  const [currentCrossword, setCurrentCrossword] = useState(GrowingGreen);
   return (
     <div className="Crossword-Container">
-      <PageTitle>Crossword</PageTitle>
+      <PageTitle>Crossword - {currentCrossword.name}</PageTitle>
       <div className="Game-Container">
-        <ReactCrossword data={games[0]} />
-        <div>Gail Grabowski, edited by Stanley Newman</div>
+        <ReactCrossword data={currentCrossword} />
+        <div>{currentCrossword.author}</div>
       </div>
     </div>
   );
