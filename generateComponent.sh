8819fe7a3708df4af0c098e5aee4274f40a85a2d
Alexander Burdiss <aburdiss@icloud.com>
2021-09-07 23:07:32 -0400

    Created Bash Script to generate a react component, and hooked up to package.json

---
+++ generateComponent.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# generateComponent.sh
+# Author: Alexander Burdiss
+# Since: 9/7/21
+# Version: 1.0.0
+# Description: Generates a React component and all the necessary files that go
+# along with it.
+
+path=$1
+component=$2
+date=`date +"%D"`
+
+if [[ ! -d "src/$path" ]]; then
+  echo
+  echo "Directory src/$path Doesn't exist!"
+  echo
+  exit 1
+elif [[ -d "src/$path/$component" ]]; then
+  echo
+  echo "Component src/$path/$component Already exists!"
+  echo
+  exit 1
+fi
+
+mkdir "src/$path/$component"
+
+# Make JS File
+echo "import React from \"react\";
+import \"./$component.scss\";
+
+/**
+ * @namespace $component
+ * @function $component
+ * @author Alexander Burdiss
+ * @since $date
+ * @version 1.0.0
+ * @component
+ */
+export default function $component() {
+  return (
+    <div className=\"$component-Container\">
+      <div>$component Works!</div>
+    </div>
+  );
+}" > "src/$path/$component/$component.js"
+
+# Make SCSS File
+touch "src/$path/$component/$component.scss"
+
+# Make Storybook File
+echo "import React from \"react\";
+import $component from \"./$component\";
+
+export default {
+  title: \"$path/$component\",
+  component: $component,
+  argTypes: {},
+};
+
+const Template = (args) => <$component {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};" > "src/$path/$component/$component.stories.js"
+
+# Make Jest test file
+echo "import { render } from \"@testing-library/react\";
+import $component from \"./$component\";
+
+describe(\"renders $component\", () => {
+  test(\"renders base component\", () => {
+    render(<$component />);
+  });
+});" > "src/$path/$component/$component.test.js"
+
+echo
+echo "Component src/$path/$component Created Successfuly!"
+echo
+exit 0
