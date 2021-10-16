5f6adf8c4a7989604927576c7c87a9ec39a144f5
Alexander Burdiss <aburdiss@icloud.com>
2021-10-15 20:20:03 -0400

    Add additional options to SEO component

--- src/Components/Structural/SEO/SEO.js
+++ src/Components/Structural/SEO/SEO.js
@@ -6,11 +6,19 @@ import { Helmet } from "react-helmet";
  * @function SEO
  * @author Alexander Burdiss
  * @since 10/14/21
- * @version 1.0.0
+ * @version 1.1.0
  * @component
  */
-export default function SEO({ title }) {
+export default function SEO({ title, description, style }) {
   return (
-    <Helmet>{title && <title>{title} | The Bedtime Project</title>}</Helmet>
+    <Helmet>
+      {description && <meta name="description" content={description} />}
+      {title ? (
+        <title>{title} | The Bedtime Project</title>
+      ) : (
+        <title>The Bedtime Project</title>
+      )}
+      {style && <style type="text/css">{style}</style>}
+    </Helmet>
   );
 }
