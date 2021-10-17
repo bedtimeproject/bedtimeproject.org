7152fb76361738d8bc30e3f385870ed078ee5f46
Alexander Burdiss <aburdiss@icloud.com>
2021-10-17 03:05:35 -0400

    Restyle Rainy Windows

--- src/Components/General/RainyWindow/RainyWindow.js
+++ src/Components/General/RainyWindow/RainyWindow.js
@@ -52,48 +52,21 @@ export default function RainyWindow({ right }) {
         </div>
         <div className="window top"></div>
         <div className="window bottom"></div>
-        <div className="ac">
-          <div className="unit">
-            <div className="vent small">
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-            </div>
-            <div className="vent small">
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-            </div>
-            <div className="controls"></div>
-            <div className="vent large">
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-              <div className="slot"></div>
-            </div>
-          </div>
-        </div>
-        <div className="curtain-rod"></div>
-        {!right && <div className="curtain left"></div>}
-        {right && <div className="curtain right"></div>}
         <div className="sill">
-          <div className="items">
-            <div className="planter">
-              <div className="leaf"></div>
-              <div className="leaf"></div>
-              <div className="leaf"></div>
+          {right && (
+            <div className="items">
+              <div className="planter">
+                <div className="leaf"></div>
+                <div className="leaf"></div>
+                <div className="leaf"></div>
+              </div>
+              <div className="books">
+                <div className="book"></div>
+                <div className="book"></div>
+                <div className="book"></div>
+              </div>
             </div>
-            <div className="books">
-              <div className="book"></div>
-              <div className="book"></div>
-              <div className="book"></div>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
