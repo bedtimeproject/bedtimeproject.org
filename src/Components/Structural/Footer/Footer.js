a2ba396b982c70918bf614ed2d2351a8d2c426ad
Alexander Burdiss <aburdiss@icloud.com>
2021-11-19 22:22:32 -0500

    Update Footer so that the Privacy Policy Link will never be on top of copyright symbol.

--- src/Components/Structural/Footer/Footer.js
+++ src/Components/Structural/Footer/Footer.js
@@ -1,3 +1,4 @@
+// @ts-check
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.scss";
@@ -7,12 +8,10 @@ import "./Footer.scss";
  * @description The footer for the site.
  * @author Alexander Burdiss
  * @since 5/13/21
- * @version 1.0.0
+ * @version 1.0.1
  * @component
  * @example
- * ```jsx
  * <Footer />
- * ```
  */
 export default function Footer() {
   return (
