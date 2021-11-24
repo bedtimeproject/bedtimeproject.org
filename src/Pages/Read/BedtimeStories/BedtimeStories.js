09e1f9b39fbae9a07182582e5681e2a961286e8e
Alexander Burdiss <aburdiss@icloud.com>
2021-11-23 22:02:37 -0500

    Update Bedtime Stories page to use Bookshelf

--- src/Pages/Read/BedtimeStories/BedtimeStories.js
+++ src/Pages/Read/BedtimeStories/BedtimeStories.js
@@ -2,36 +2,51 @@
 import React from "react";
 import "./BedtimeStories.scss";
 
-import StoryButton from "../../../Components/Buttons/StoryButton/StoryButton";
+import BookLink from "../../../Components/Buttons/BookLink/BookLink";
+import Bookshelf from "../../../Components/General/Bookshelf/Bookshelf";
 import StandardWrapper from "../../../Components/Structural/StandardWrapper/StandardWrapper";
 import SEO from "../../../Components/Structural/SEO/SEO";
-import PageTitle from "../../../Components/Structural/PageTitle/PageTitle";
 
 /**
  * @namespace BedtimeStories
  * @function BedtimeStories
  * @author Alexander Burdiss
  * @since 10/17/21
- * @version 1.0.0
+ * @version 2.0.0
  * @component
  */
 export default function BedtimeStories() {
+  const shelf1Books = [
+    {
+      title: "Mrs. Blue Sky",
+      link: "/read/bedtime-stories/mrs-blue-sky",
+      field_author: "C. A. Moonbloom",
+      backgroundColor: "#133061",
+    },
+    {
+      title: "The Lady and the Frog",
+      link: "read/bedtime-stories/the-lady-and-the-frog",
+      field_author: "Daniel Stigmon",
+      backgroundColor: "#dab656",
+    },
+  ];
   return (
     <StandardWrapper>
       <div className="BedtimeStories-Container">
         <SEO title="Bedtime Stories" />
-        <PageTitle>Bedtime Stories</PageTitle>
-        <div className="BedtimeStories-Button-Container">
-          <StoryButton link="/read/bedtime-stories/the-lady-and-the-frog">
-            The Lady and the Frog
-          </StoryButton>
-          <StoryButton link="/read/bedtime-stories/mrs-blue-sky">
-            Mrs. Blue Sky
-          </StoryButton>
-          <StoryButton link="/read/bedtime-stories/the-guide-to-sunset">
-            The Guide to Sunset
-          </StoryButton>
-        </div>
+        <Bookshelf
+          pageTitle="Bedtime Stories"
+          books1={shelf1Books.map((book, index) => (
+            <BookLink
+              key={index}
+              link={book.link}
+              story={book}
+              backgroundColor={book.backgroundColor}
+            />
+          ))}
+          books2={null}
+          books3={null}
+        />
       </div>
     </StandardWrapper>
   );
