eb6b8c10d124133d2c7e378ac21686a48928fa90
Alexander Burdiss <aburdiss@icloud.com>
2022-03-22 23:12:51 -0400

    Removes normalize.css as npm dependency, adds as local file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { HelmetProvider } from "react-helmet-async";
 // Polyfills
 import "core-js/features/string/replace-all";
 
-import "normalize.css";
+import "./normalize.css";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./Contexts/AppContext";
