294239690e501ad1f849012d10d68b815d6c8078
Alexander Burdiss <aburdiss@icloud.com>
2022-06-02 21:18:41 -0400

    Removes core-js dependency

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { HelmetProvider } from "react-helmet-async";
 
-// Polyfills
-import "core-js/features/string/replace-all";
-
 import "./normalize.css";
 import "./index.css";
+
 import App from "./App";
+
 import { AppProvider } from "./Contexts/AppContext";
+
+import { polyfills } from "./polyfills";
+polyfills();
 
 ReactDOM.render(
   <React.StrictMode>
